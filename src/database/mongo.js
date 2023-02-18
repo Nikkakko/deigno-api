929955bb0f07c3f22acd039c9a9c1fbfee389a72
Nikkakko <nikolozkopadze@gmail.com>
2023-02-18 22:14:35 +0400

    fixed

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -8,7 +8,10 @@ const url = process.env.MONGO_URI;
 const connect = async () => {
   try {
     mongoose.set('strictQuery', true);
-    await mongoose.connect(url, {});
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('MongoDB connected');
   } catch (error) {
     console.log(error);
